5aa6e209d120d10b74b35b9daed28531d8d82909
ardimr <rizkyardimaulana@gmail.com>
2021-05-07 23:01:50 +0700

    fixing transfer money

--- gui/client.py
+++ gui/client.py
@@ -60,12 +60,12 @@ def Signin(_username, _password) :
         status = 1
     elif (response.status_code == 401) : #Wrong Password
         status = 2
-    #saving token session    
+    #saving var.token session    
     print("Posting to " + url)
     buffer = response.json()
-    token = buffer['accessToken']
+    var.token = buffer['accessvar.token']
     print(form)
-    print(token)
+    print(var.token)
     return status
 
 def CreateBill(bill_url, _bill_name, _recipient, _amount, _description):
@@ -80,16 +80,16 @@ def CreateBill(bill_url, _bill_name, _recipient, _amount, _description):
     form_json = json.dumps(form)
 
     #Post to server
-    response = requests.post(bill_url,json=form_json, headers = token)
+    response = requests.post(bill_url,json=form_json, headers = var.token)
     print('Creating bill to ' + bill_url)
 
-def GetBill(_username, _bill_id, token) :
+def GetBill(_username, _bill_id, var.token) :
     if(_bill_id == ''):
         url = _bill_URL + '/' + _username
     else :
         url = _bill_URL +'/'+ _username +'/'+_bill_id
     
-    response = requests.get(url, headers = token)
+    response = requests.get(url, headers = var.token)
     
     #Return response as Json data
     bill_json = response.json().load()
@@ -98,32 +98,32 @@ def GetBill(_username, _bill_id, token) :
 
 def UpdateBill(_bill_id) :
     url = _bill_URL +'/'+ _bill_id
-    response = requests.put(url, headers = token)
+    response = requests.put(url, headers = var.token)
 
 def DeleteBill(_bill_id):
     url = _bill_URL +'/'+ _bill_id
-    response = requests.delete(url, headers = token)
+    response = requests.delete(url, headers = var.token)
 
 
-def GetCredit (credit_url, _username, token) :
+def GetCredit (credit_url, _username, var.token) :
     #Get Credit from server
     url = credit_url + _username
-    response = requests.get(url, headers = token)
+    response = requests.get(url, headers = var.token)
 
-    credit = response.json().load()
-    return(credit[amount_credit])
+    credit = response.json()
+    return(credit['amount_credit'])
 
-def ChangeCredit(credit_url, _username,_amount_, token):
+def ChangeCredit(credit_url, _username,_amount_, var.token):
     #Put new data to server
     url = credit_url + _username
-    response = requests.put(url, _amount_, headers = token)
+    response = requests.put(url, _amount_, headers = var.token)
 
-def TransferMoney(_recipient, _amount_, _description,token):
+def TransferMoney(_recipient, _amount_, _description,var.token):
     credit_url = _user_URL + '/'
     #Get recipient credit
-    recipient_credit = GetCredit(credit_url, _recipient, token)
+    recipient_credit = GetCredit(credit_url, _recipient, var.token)
     #get sender credit
-    sender_credit = GetCredit(credit_url, username, token)
+    sender_credit = GetCredit(credit_url, username, var.token)
     
     #adding recipient credit
     recipient_credit_buffer = recipient_credit + _amount_
@@ -131,9 +131,9 @@ def TransferMoney(_recipient, _amount_, _description,token):
     sender_credit_buffer = sender_credit - _amount_
     
     #Change Recipient Credit
-    ChangeCredit(credit_url, _recipient, recipient_credit_buffer, token)
+    ChangeCredit(credit_url, _recipient, recipient_credit_buffer, var.token)
     #Change Sender Credit
-    ChangeCredit(credit_url, username, sender_credit_buffer, token)
+    ChangeCredit(credit_url, username, sender_credit_buffer, var.token)
 
     print("Transfering Money Rp"+str(_amount_)+" to "+_recipient)
 
