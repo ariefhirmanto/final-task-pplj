7de2b8e0f4c7d0e359876dfe270a67752749eb58
Arief Hirmanto <ariefhirmanto7@gmail.com>
2021-05-09 12:06:38 +0700

    resolve update bill

--- api/app/controllers/user.controller.js
+++ api/app/controllers/user.controller.js
@@ -51,7 +51,7 @@ exports.findOne = (req, res) => {
 };
 
 exports.updateBill = (req, res) => {
-  const username = req.body.bill_owner;
+  const username = req.body.username;
   const new_bill = req.body.bill_id;
   User.findOneAndUpdate({ username: username}, { $push: { bill_id: new_bill}}, {new: true, upsert: true, useFindAndModify: false })
   .then(data => {
